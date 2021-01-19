87415a5fbf6d86ec485d4fc80c766ceb69668592
Vitaly D <admin@netmin.ru>
2021-01-19 19:20:44 +0500

    create cache forecast in memory

--- src/app/app/services/openweather.py
+++ src/app/app/services/openweather.py
@@ -2,8 +2,23 @@ import httpx
 import os
 from typing import Optional
 
+from ..infrastructure.weather_cache import get_weather, set_weather
+
+
+from logging import getLogger
+
+_logger = getLogger(__name__)
+
 
 async def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
+    _logger.info('INFO')
+    _logger.warning('WARN')
+    _logger.error('ERR')
+    _logger.debug('Debug')
+    if forecast := get_weather(city, state, country, units):
+        _logger.error(f'FORECAST {forecast}')
+        return forecast
+
     base_url = 'https://api.openweathermap.org/data/2.5/weather'
     api_key = os.getenv('API_KEY', 'test_key')
     q = f'{city},{state},{country}' if state else f'{city},{country}'
@@ -15,5 +30,7 @@ async def get_report(city: str, state: Optional[str], country: str, units: str) 
 
     data = resp.json()
     forecast = data['main']
+
+    set_weather(city, state, country, units, forecast)
 
     return forecast
