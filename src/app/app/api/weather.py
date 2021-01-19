0b6e1d3a6616087198028e834aaf0e476cf32c19
Vitaly D <admin@netmin.ru>
2021-01-19 19:43:55 +0500

    validation data for request data

--- src/app/app/api/weather.py
+++ src/app/app/api/weather.py
@@ -4,6 +4,7 @@ import fastapi
 from fastapi import Depends
 
 from ..models.location import Location
+from ..models.validation_error import ValidationError
 from ..services import openweather
 
 router = fastapi.APIRouter()
@@ -11,5 +12,11 @@ router = fastapi.APIRouter()
 
 @router.get('/api/weather/{city}')
 async def get_weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
-    report = await openweather.get_report(loc.city, loc.state, loc.country, units)
-    return report
+    try:
+        return await openweather.get_report(loc.city, loc.state, loc.country, units)
+
+    except ValidationError as err:
+        return fastapi.Response(content=err.error_msg, status_code=err.status_code)
+
+    except Exception as err:
+        return fastapi.Response(content='Error processing your request.', status_code=500)
--- src/app/app/services/openweather.py
+++ src/app/app/services/openweather.py
@@ -1,8 +1,11 @@
 import httpx
 import os
-from typing import Optional
+from typing import Optional, Tuple
+
+from httpx import Response
 
 from ..infrastructure.weather_cache import get_weather, set_weather
+from ..models.validation_error import ValidationError
 
 
 from logging import getLogger
@@ -11,10 +14,8 @@ _logger = getLogger(__name__)
 
 
 async def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    _logger.info('INFO')
-    _logger.warning('WARN')
-    _logger.error('ERR')
-    _logger.debug('Debug')
+    city, state, country, units = validate_units(city, state, country, units)
+
     if forecast := get_weather(city, state, country, units):
         _logger.error(f'FORECAST {forecast}')
         return forecast
@@ -25,8 +26,9 @@ async def get_report(city: str, state: Optional[str], country: str, units: str) 
     url = f'{base_url}?q={q}&appid={api_key}&units={units}'
 
     async with httpx.AsyncClient() as client:
-        resp = await client.get(url)
-        resp.raise_for_status()
+        resp: Response = await client.get(url)
+        if resp.status_code != 200:
+            raise ValidationError(resp.text, resp.status_code)
 
     data = resp.json()
     forecast = data['main']
@@ -34,3 +36,33 @@ async def get_report(city: str, state: Optional[str], country: str, units: str) 
     set_weather(city, state, country, units, forecast)
 
     return forecast
+
+
+def validate_units(city: str, state: Optional[str], country: Optional[str], units: str) -> \
+        Tuple[str, Optional[str], str, str]:
+    city = city.lower().strip()
+    if not country:
+        country = "ru"
+    else:
+        country = country.lower().strip()
+
+    if len(country) != 2:
+        error = f"Invalid country: {country}. It must be a two letter abbreviation such as US or RU."
+        raise ValidationError(status_code=400, error_msg=error)
+
+    if state:
+        state = state.strip().lower()
+
+    if state and len(state) != 2:
+        error = f"Invalid state: {state}. It must be a two letter abbreviation such as CA or KS (use for US only)."
+        raise ValidationError(status_code=400, error_msg=error)
+
+    if units:
+        units = units.strip().lower()
+
+    valid_units = {'standard', 'metric', 'imperial'}
+    if units not in valid_units:
+        error = f"Invalid units '{units}', it must be one of {valid_units}."
+        raise ValidationError(status_code=400, error_msg=error)
+
+    return city, state, country, units
